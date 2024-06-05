705048235ace2215816272dab29a4664469c8da9
kms8032 <alstjr803232@g.yju.ac.kr>
2024-06-05 12:10:27 +0900

    2nd commit

--- 9-2.py
+++ 9-2.py
@@ -1,9 +1,17 @@
 num = input("주민번호를 입력하세요.:")
 
+num_list = []
+check = [2,3,4,5,6,7,8,9,2,3,4,5]
 
 for i in num :
-    for x in range(2,9):
-        cal = int(i) * x
+    num_list.append(int(i))
+
+result = [] 
+for a, b in zip(num_list, check):
+    result.append(a*b)
     
-    
-print(cal)
+result = 11 - (sum(result)%11)
+if result == num_list[12] :
+    print("유효한 주민번호입니다.")
+else :
+    print("유효하지 않은 주민번호입니다.")
